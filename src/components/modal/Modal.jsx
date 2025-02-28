76eb772ea35fcb15ae7601eed93b83a6ebc46f50
Shir5 <Vova0120_black@icloud.com>
2025-02-28 18:18:37 +0300

    pagination, table fix, modal delete button addition

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Modal.module.css';
 import { Button } from "@mui/material";
 
-const Modal = ({ setIsModal, isModal, selectedItem }) => {
+const Modal = ({ setIsModal, isModal, selectedItem, handleDelete }) => {
     return (
         isModal && (
             <div className={styles.modalBackdrop} onClick={() => setIsModal(false)}>
@@ -11,13 +11,22 @@ const Modal = ({ setIsModal, isModal, selectedItem }) => {
                     <p><strong>Длина:</strong> {selectedItem.length}</p>
                     <p><strong>Сложность:</strong> {selectedItem.difficulty}</p>
                     <p><strong>Описание:</strong> {selectedItem.description}</p>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => setIsModal(false)}
-                    >
-                        Закрыть
-                    </Button>
+                    <div className={styles.buttonGroup}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => setIsModal(false)}
+                        >
+                            Закрыть
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => handleDelete(selectedItem.id)}
+                        >
+                            Удалить
+                        </Button>
+                    </div>
                 </div>
             </div>
         )
